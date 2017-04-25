d51178c8b56b4f8dd5453b97cfc037734c87752f
Mario Galic <mario.galic@guardian.co.uk>
2017-04-25 10:36:30 +0100

    Disable username anonymisation

--- nodejs/deploy.sh
+++ nodejs/deploy.sh
@@ -24,7 +24,6 @@ aws lambda update-function-code --function-name userIsNotSubscriber --s3-bucket 
 aws lambda update-function-code --function-name isAutoDeletionCriteriaSatisfied --s3-bucket $S3_BUCKET --s3-key $S3_KEY
 aws lambda update-function-code --function-name notifyUserhelp --s3-bucket $S3_BUCKET --s3-key $S3_KEY
 aws lambda update-function-code --function-name deleteUser --s3-bucket $S3_BUCKET --s3-key $S3_KEY
-aws lambda update-function-code --function-name anonymiseUsername --s3-bucket $S3_BUCKET --s3-key $S3_KEY
 aws lambda update-function-code --function-name userIsValidated --s3-bucket $S3_BUCKET --s3-key $S3_KEY
 
 echo "Done."
